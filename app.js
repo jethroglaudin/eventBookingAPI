e6ea05184034abe446763413004d0a68ceb80fdc
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-27 23:23:08 -0400

    created RootQuery and RootMutation schema type.

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 const chalk = require("chalk");
 
 const app = express();
 
 app.use(bodyParser.json());
-app.get('/', (req, res, next) => {
-    res.send('Hello World');
-})
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["Romantic Coooking", "Sailing", "All-Night Coding"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    }
+  })
+);
 
 const PORT = process.env.Port || 3000;
 app.listen(PORT, err => {

9c01eac847e37be5083be4e72269de83a9da1feb
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-28 23:48:40 -0400

    included eventInput property within resolvers

--- app.js
+++ app.js
@@ -10,54 +10,52 @@ const events = [];
 
 app.use(bodyParser.json());
 app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: buildSchema(`
-        type Event {
+    '/graphql',
+    graphqlHttp({
+      schema: buildSchema(`
+          type Event {
             _id: ID!
             title: String!
             description: String!
             price: Float!
             date: String!
-        }
-       
-        input EventInput {
-            title: String
+          }
+          input EventInput {
+            title: String!
             description: String!
             price: Float!
             date: String!
+          }
+          type RootQuery {
+              events: [Event!]!
+          }
+          type RootMutation {
+              createEvent(eventInput: EventInput): Event
+          }
+          schema {
+              query: RootQuery
+              mutation: RootMutation
+          }
+      `),
+      rootValue: {
+        events: () => {
+          return events;
+        },
+        createEvent: args => {
+          const event = {
+            _id: Math.random().toString(),
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: args.eventInput.date
+          };
+          events.push(event);
+          return event;
         }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
-
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
-    `),
-    rootValue: {
-      events: () => {
-        return events;
       },
-      createEvent: args => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.title,
-          description: args.description,
-          price: +args.price,
-          data: new Date().toISOString()
-        };
-        events.push(event);
-      }
-    },
-    graphiql: true
-  })
-);
+      graphiql: true
+    })
+  );
 
 const PORT = process.env.Port || 3000;
 app.listen(PORT, err => {

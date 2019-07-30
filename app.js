25b71a8a65634504e93b4e8b1c615bae77cb0449
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-07-29 23:44:08 -0400

    conected to mongoDB

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
 const chalk = require("chalk");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -10,52 +11,61 @@ const events = [];
 
 app.use(bodyParser.json());
 app.use(
-    '/graphql',
-    graphqlHttp({
-      schema: buildSchema(`
-          type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-          }
-          input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-          }
-          type RootQuery {
-              events: [Event!]!
-          }
-          type RootMutation {
-              createEvent(eventInput: EventInput): Event
-          }
-          schema {
-              query: RootQuery
-              mutation: RootMutation
-          }
-      `),
-      rootValue: {
-        events: () => {
-          return events;
-        },
-        createEvent: args => {
-          const event = {
-            _id: Math.random().toString(),
-            title: args.eventInput.title,
-            description: args.eventInput.description,
-            price: +args.eventInput.price,
-            date: args.eventInput.date
-          };
-          events.push(event);
-          return event;
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+        type Event {
+          _id: ID!
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
         }
+        input EventInput {
+          title: String!
+          description: String!
+          price: Float!
+          date: String!
+        }
+        type RootQuery {
+            events: [Event!]!
+        }
+        type RootMutation {
+            createEvent(eventInput: EventInput): Event
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
       },
-      graphiql: true
-    })
-  );
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
+mongoose.connect(
+  `mongodb+srv://${process.env.MONGO_USER}:${
+    process.env.MONGO_PASSWORD
+  }@myexpressapp-jlald.mongodb.net/test?retryWrites=true&w=majority`
+).then(() => {
+    console.log(chalk.green.inverse("MongoDB Connected"));
+}).catch(err => {
+    console.log(err)
+})
 
 const PORT = process.env.Port || 3000;
 app.listen(PORT, err => {

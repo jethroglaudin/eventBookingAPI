56ff7d1e71c3b6603c0b5136a76e4c596ec70ab1
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-04 20:21:59 -0400

    crated resolvers and schema folder

---
+++ graphql/resolvers/resolvers.js
@@ -0,0 +1,116 @@
+const bcrypt = require("bcryptjs");
+
+// Import Model
+const Event = require("../../models/event");
+
+// Import User Model
+const User = require("../../models/user");
+
+const events = eventIds => {
+  // look for through events where ID is in a list of ids.
+  return Event.find({ _id: { $in: eventIds } })
+    .then(events => {
+      return events.map(event => {
+        return {
+          ...event._doc,
+          _id: event.id,
+          creator: user.bind(this, event.creator)
+        };
+      });
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+const user = userId => {
+  return User.findById(userId)
+    .then(user => {
+      return {
+        ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user.createdEvents)
+      };
+    })
+    .catch(err => {
+      throw err;
+    });
+};
+
+module.exports = {
+  events: () => {
+    return Event.find()
+      .then(events => {
+        return events.map(event => {
+          // convert to normal strong that's understood by graphql
+          return {
+            ...event._doc,
+            _id: event.id,
+            creator: user.bind(this, event.creator)
+          };
+        });
+      })
+      .catch(err => {
+        throw err;
+      });
+  },
+  createEvent: args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: "5d47586cd7ed9a80536cfffe"
+    });
+    let createdEvent;
+    return event
+      .save()
+      .then(result => {
+        createdEvent = {
+          ...result._doc,
+          _id: result._doc._id.toString(),
+          creator: user.bind(this, result.creator)
+        };
+        return User.findById("5d47586cd7ed9a80536cfffe");
+        console.log(result);
+        //gets all core properties that makes event object and leavse out meta data
+      })
+      .then(user => {
+        // check if user
+        if (!user) {
+          throw new Error("User not found.");
+        }
+        user.createdEvents.push(event);
+        return user.save();
+      })
+      .then(result => {
+        return createdEvent;
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+  },
+  createUser: args => {
+    return User.findOne({ email: args.userInput.email })
+      .then(user => {
+        if (user) {
+          throw new Error("User exists already.");
+        }
+        return bcrypt.hash(args.userInput.password, 12);
+      })
+      .then(hashedPassword => {
+        const user = new User({
+          email: args.userInput.email,
+          password: hashedPassword
+        });
+        return user.save();
+      })
+      .then(result => {
+        return { ...result._doc, password: null, _id: result.id };
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
+};

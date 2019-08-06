a9c416c5c9bb486580ba2ae7d9a81647528d2185
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-05 23:07:13 -0400

    created bookEvent function to fetch events and allow users to book events

--- graphql/resolvers/resolvers.js
+++ graphql/resolvers/resolvers.js
@@ -121,5 +121,19 @@ module.exports = {
     } catch (err) {
       throw err;
     }
+  },
+  bookEvent: async args => {
+    const fetchedEvent = await Event.findOne({ _id: args.eventId });
+    const booking = new Booking({
+      user: "5d478568c711283ecf93cb53",
+      event: fetchedEvent
+    });
+    const result = await booking.save();
+    return {
+        ...result._doc,
+      _id: result.id,
+      createdAt: new Date(result.createdAt).toISOString(),
+      updatedAt: new Date(result.updatedAt).toISOString()
+    };
   }
 };

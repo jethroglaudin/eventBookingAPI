3f7a1b2325d10e6c43efbda6936aef84539f7958
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-05 22:51:48 -0400

    updated booking.js added event and users object fields

---
+++ models/booking.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookingSchema = new Schema(
+  {
+    event: {
+      type: Schema.Types.ObjectId,
+      ref: "Event"
+    },
+    users: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    }
+    // creates created at and updated at for every entry.
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Booking", bookingSchema);

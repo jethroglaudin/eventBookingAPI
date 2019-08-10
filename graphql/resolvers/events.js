2ca5d1f36a3b9cdd45f7ee7f711f58a57fcfd6a6
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-10 19:35:03 -0400

    created events.js creating events

---
+++ graphql/resolvers/events.js
@@ -0,0 +1,44 @@
+const Event = require("../../models/event");
+const { transformEvent } = require("./merge");
+
+module.exports = {
+  events: async () => {
+    try {
+      const events = await Event.find();
+      return events.map(event => {
+        // convert to normal strong that's understood by graphql
+        return transformEvent(event);
+      });
+    } catch (err) {
+      throw err;
+    }
+  },
+  createEvent: async args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: "5d478568c711283ecf93cb53"
+    });
+    let createdEvent;
+    try {
+      const result = await event.save();
+      createdEvent = transformEvent(result);
+      const creator = await User.findById("5d478568c711283ecf93cb53");
+      console.log(result);
+      //gets all core properties that makes event object and leavse out meta data
+
+      // check if user
+      if (!creator) {
+        throw new Error("User not found.");
+      }
+      creator.createdEvents.push(event);
+      await creator.save();
+      return createdEvent;
+    } catch (err) {
+      console.log(err);
+      throw err;
+    }
+  }
+};

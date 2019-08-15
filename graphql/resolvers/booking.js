0b89c299e3b1ac94afe0f78dedaef397730ed41d
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-14 22:50:26 -0400

    updated all resolvers with isAuth so that it only able to be used if user logged in and authenticated

--- graphql/resolvers/booking.js
+++ graphql/resolvers/booking.js
@@ -1,10 +1,13 @@
 const Booking = require("../../models/booking");
-const Event = require('../../models/event');
-const { transformBooking, transformEvent } = require('./merge');
-
+const Event = require("../../models/event");
+const { transformBooking, transformEvent } = require("./merge");
 
 module.exports = {
-  bookings: async () => {
+  bookings: async (args, req) => {
+    // check if authenticated
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
     try {
       const bookings = await Booking.find();
       return bookings.map(booking => {
@@ -14,16 +17,24 @@ module.exports = {
       throw err;
     }
   },
-  bookEvent: async args => {
+  bookEvent: async (args, req) => {
+    // check if authenticated
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
     const fetchedEvent = await Event.findOne({ _id: args.eventId });
     const booking = new Booking({
       user: "5d478568c711283ecf93cb53",
       event: fetchedEvent
     });
     const result = await booking.save();
-    return transformBooking(result)
+    return transformBooking(result);
   },
-  cancelBooking: async args => {
+  cancelBooking: async (args, req) => {
+    // check if authenticated
+    if (!req.isAuth) {
+      throw new Error("Unauthenticated");
+    }
     try {
       const booking = await Booking.findById(args.bookingId).populate("event");
       const event = transformEvent(booking.event);

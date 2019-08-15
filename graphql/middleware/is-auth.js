2e3f82bd7a3f91f60b0d0aa943dd4401a15692a8
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-14 21:36:19 -0400

    created middleware that makes checks to ensure we have a valid token.

---
+++ graphql/middleware/is-auth.js
@@ -0,0 +1,30 @@
+module.exports = (req, res, next) => {
+  // this sees if theres is an authorization field within the incoming request
+  const authHeader = req.get("Authorization");
+  if (!authHeader) {
+    req.isAuth = false;
+    return next();
+  }
+  // split auth header on the white space
+  const token = authHeader.split(" ")[1]; // bearer token
+  if (!token || token === "") {
+    req.isAuth = false;
+    return next();
+  }
+  let decodedToken;
+  try {
+    // decoded token
+    decodedToken = jwt.verify(token, "somesupersecretkey");
+  } catch (err) {
+    req.isAuth = false;
+    return next;
+  }
+
+  if(!decodedToken){
+      req.isAuth = false;
+      return next();
+  }
+  req.isAuth = true;
+  req.userId = decodedToken.userId;
+  next();
+};

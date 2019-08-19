76a04b9fc792cd9b29eb67061492e9ffcdfbf23a
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-18 23:22:18 -0400

    created Bookings and Events.js

---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class EventsPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default EventsPage;

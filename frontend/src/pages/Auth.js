20f0e6f762a1d9d6b83a03adfa5f59ee295ef286
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-18 23:18:21 -0400

    updated Auth.js

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react'
 
- class Auth extends Component {
+ class AuthPage extends Component {
     render() {
         return (
             <div>
-                
+                <h1>The Auth Page!</h1>
             </div>
         )
     }
 }
 
-export default Auth;
+export default AuthPage;

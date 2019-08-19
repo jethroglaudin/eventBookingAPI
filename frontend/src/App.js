55e915cd906114603b38b0d504165cbf7275d2cd
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-18 23:11:26 -0400

    imported BrowserRouter from react-router-dom

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <h1>It works!</h1>
-    </div>
+    </Router>
   );
 }
 

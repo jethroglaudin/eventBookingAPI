b307905e565f601f6e8bc3525d31009c2a66113f
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-22 15:06:52 -0400

    imported the navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,20 +7,24 @@ import {
 } from "react-router-dom";
 
 import AuthPage from "./pages/Auth";
-import BookingsPage from './pages/Bookings';
-import EventsPage from './pages/Events';
+import BookingsPage from "./pages/Bookings";
+import EventsPage from "./pages/Events";
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import "./App.css";
 
 function App() {
   return (
     <Router>
-      <Switch>
-        <Redirect path="/" to="/auth" exact />
-        <Route path="/auth" component={AuthPage} />
-        <Route path="/events" component={EventsPage} />
-        <Route path="/bookings" component={BookingsPage} />
-      </Switch>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Redirect path="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </main>
     </Router>
   );
 }

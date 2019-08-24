0d9e7f3ec311f6d47f61f98ea3116e82201bf3aa
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-24 00:26:20 -0400

    created constructor that received props and created two references.

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -3,16 +3,25 @@ import React, { Component } from "react";
 import './Auth.css';
 
 class AuthPage extends Component {
+    constructor(props) {
+        super(props);
+        this.emailEl = React.createRef();
+        this.passwordEl = React.createRef();
+    }
+
+    submitHandler = () => {
+
+    };
   render() {
     return (
        <form className="auth-form">
         <div className="form-control">
         <label htmlFor="email">E-Mail</label>
-          <input type="email" id="email" />
+          <input type="email" id="email" ref= { this.emailEl } />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" />
+          <input type="password" id="password" ref= { this.passwordEl } />
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>

c45e2cff8663b57f96c7cfb07127b1e427b795d2
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-23 23:52:01 -0400

    imported auth.css

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
+import './Auth.css';
+
 class AuthPage extends Component {
   render() {
     return (
-       <form>
+       <form className="auth-form">
         <div className="form-control">
-          <label htmlFor="email">E-Mail</label>
+        <label htmlFor="email">E-Mail</label>
           <input type="email" id="email" />
         </div>
         <div className="form-control">
@@ -14,7 +16,7 @@ class AuthPage extends Component {
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
-          <button type="button" Switch to signup>
+          <button type="button"> Switch to Signup
           </button>
         </div>
       </form>

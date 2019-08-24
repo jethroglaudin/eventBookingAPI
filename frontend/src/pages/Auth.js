6f5031137bf277f848df407c461bc65dc2de5e86
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-24 00:49:06 -0400

    set query mutation for createUser passing in email and password inputs. Also made fetch POST request to the server

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -1,42 +1,59 @@
 import React, { Component } from "react";
 
-import './Auth.css';
+import "./Auth.css";
 
 class AuthPage extends Component {
-    constructor(props) {
-        super(props);
-        this.emailEl = React.createRef();
-        this.passwordEl = React.createRef();
+  constructor(props) {
+    super(props);
+    this.emailEl = React.createRef();
+    this.passwordEl = React.createRef();
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    const email = this.emailEl.current.value;
+    const password = this.passwordEl.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      // either not email or no passord then we just return and do not continue
+      return;
     }
 
-    submitHandler = () => {
-        const email = this.emailEl.current.value;
-        const password = this.passwordEl.current.value;
-
-        if(email.trim().length === 0 || password.trim().length === 0) {
-            // either not email or no passord then we just return and do not continue
-            return;
-        }
-
-        console.log(email, password);
-
-
+    const requestBody = {
+      query: `
+            mutation {
+                createUser(userInput: {email: "${email}", password: "${password}"}) {
+                    _id
+                    email
+                }
+            }
+            
+            
+            `
     };
+
+    fetch('http://localhost:4000/graphql', {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+  };
   render() {
     return (
-       <form className="auth-form" onSubmit={this.submitHandler}>
+      <form className="auth-form" onSubmit={this.submitHandler}>
         <div className="form-control">
-        <label htmlFor="email">E-Mail</label>
-          <input type="email" id="email" ref= { this.emailEl } />
+          <label htmlFor="email">E-Mail</label>
+          <input type="email" id="email" ref={this.emailEl} />
         </div>
         <div className="form-control">
           <label htmlFor="password">Password</label>
-          <input type="password" id="password" ref= { this.passwordEl } />
+          <input type="password" id="password" ref={this.passwordEl} />
         </div>
         <div className="form-actions">
           <button type="submit">Submit</button>
-          <button type="button"> Switch to Signup
-          </button>
+          <button type="button"> Switch to Signup</button>
         </div>
       </form>
     );

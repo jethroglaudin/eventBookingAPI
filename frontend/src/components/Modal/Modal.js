a3befea4f406f47b027d432e78126c1b7e941ab7
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-29 00:11:27 -0400

    updated modal function

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,17 +1,16 @@
-import React from 'react';
+import React from "react";
 
+import "./Modal.css";
 
 const modal = props => (
-    <div className="modal">
-        <header>{props.title}</header>
-        <section className="modal__content">
-            {props.children}
-        </section>
-        <section className="modal__actions">
-          {props.canCancel && <button className="btn">Cancel</button>}
-          {props.canConfirm &&  <button className="btn">Confirm</button>}
-        </section>
-    </div>
+  <div className="modal">
+    <header className="modal__header"><h1>{props.title}</h1></header>
+    <section className="modal__content">{props.children}</section>
+    <section className="modal__actions">
+      {props.canCancel && <button className="btn">Cancel</button>}
+      {props.canConfirm && <button className="btn">Confirm</button>}
+    </section>
+  </div>
 );
 
 export default modal;

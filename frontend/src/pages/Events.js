82ffe9ccbafce67d85df2f0ec911b096697d7cd1
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-29 00:10:31 -0400

    imported Modal component

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+
+import Modal from '../components/Modal/Modal';
 import './events.css';
 
  class EventsPage extends Component {
     render() {
         return (
+            <React.Fragment>
+                <Modal title="Add Event" canCancel canConfirm>
+                    <p>Modal Content</p>
+                </Modal>
             <div className="events-control">
                 <p>Share you own Events!</p>
                <button className="btn">Create Event</button>
             </div>
+            </React.Fragment>
         )
     }
 }

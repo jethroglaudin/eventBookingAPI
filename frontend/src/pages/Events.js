1795106db00a05f6b1d0efab2bf479ee39e21ba1
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-29 00:19:08 -0400

    imported Backdrop so when Modal is clicked a tint is now appears in the back

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 
 import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
 import './events.css';
 
  class EventsPage extends Component {
     render() {
         return (
             <React.Fragment>
+                <Backdrop />
                 <Modal title="Add Event" canCancel canConfirm>
                     <p>Modal Content</p>
                 </Modal>

ffa9f2d06cf016b3c6939c8ddf72c394a2ec173c
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-08-29 00:33:36 -0400

    created two seperate props methods. OnCancel and On confirm

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -7,8 +7,8 @@ const modal = props => (
     <header className="modal__header"><h1>{props.title}</h1></header>
     <section className="modal__content">{props.children}</section>
     <section className="modal__actions">
-      {props.canCancel && <button className="btn">Cancel</button>}
-      {props.canConfirm && <button className="btn">Confirm</button>}
+      {props.canCancel && <button className="btn" onClick={props.onCancel}>Cancel</button>}
+      {props.canConfirm && <button className="btn" onClick={props.onConfirm}>Confirm</button>}
     </section>
   </div>
 );

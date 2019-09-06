929d2cb20c0ba1bfe0054082c87129734e197ec3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-06 18:37:14 -0400

    created a constructor with super for reference to the input values from the form

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -9,6 +9,15 @@ import './events.css';
          creating: false
      };
 
+     // References
+     constructor(props){
+         super(props);
+         this.titleElRef = React.createRef();
+         this.priceElRef = React.createRef();
+         this.dateElRef = React.createRef();
+         this.descriptionElRef = React.createRef();
+     }
+
      startCreateEventHandler = () => {
          this.setState({creating: true});
      };
@@ -30,19 +39,19 @@ import './events.css';
                     <form>
                         <div className="form-control">
                             <label htmlFor="title">Title</label>
-                            <input type="text" id="title"/>
+                            <input type="text" id="title" ref={this.titleElRef} />
                         </div>
                         <div className="form-control">
                             <label htmlFor="price">Price</label>
-                            <input type="number" id="price"/>
+                            <input type="number" id="price" ref={this.priceElRef} />
                         </div>
                         <div className="form-control">
                             <label htmlFor="date">Date</label>
-                            <input type="date" id="date"/>
+                            <input type="date" id="date" ref={this.dateElRef} />
                         </div>
                         <div className="form-control">
                             <label htmlFor="description">Description</label>
-                            <textarea id="description" rows="4"></textarea>
+                            <textarea id="description" rows="4" ref={this.descriptionElRef} />
                         </div>
                     </form>
                 </Modal>}

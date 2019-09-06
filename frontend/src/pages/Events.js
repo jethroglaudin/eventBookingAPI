01c4a06e6f8eef47e2dad6313f32a475123294d3
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-06 18:46:34 -0400

    updated modal confirm handler with variables that store the inputs from the user. Added logic to handle if the input fields are empty. finally created an event object

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,67 +1,96 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Modal from '../components/Modal/Modal';
-import Backdrop from '../components/Backdrop/Backdrop';
-import './events.css';
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
+import "./events.css";
 
- class EventsPage extends Component {
-     state = {
-         creating: false
-     };
+class EventsPage extends Component {
+  state = {
+    creating: false
+  };
 
-     // References
-     constructor(props){
-         super(props);
-         this.titleElRef = React.createRef();
-         this.priceElRef = React.createRef();
-         this.dateElRef = React.createRef();
-         this.descriptionElRef = React.createRef();
-     }
+  // References
+  constructor(props) {
+    super(props);
+    this.titleElRef = React.createRef();
+    this.priceElRef = React.createRef();
+    this.dateElRef = React.createRef();
+    this.descriptionElRef = React.createRef();
+  }
 
-     startCreateEventHandler = () => {
-         this.setState({creating: true});
-     };
+  startCreateEventHandler = () => {
+    this.setState({ creating: true });
+  };
 
-     modalConfirmHandler = () => {
-         this.setState({creating: false});
-     };
+  modalConfirmHandler = () => {
+    this.setState({ creating: false });
+    const title = this.titleElRef.current.value;
+    const price = this.priceElRef.current.value;
+    const date = this.dateElRef.current.value;
+    const description = this.dateElRef.current.value;
 
-     modalCancelHandler = () => {
-         this.setState({creating: false});
-     }
-    render() {
-        return (
-            <React.Fragment>
-                {this.state.creating && <Backdrop />}
-
-               {this.state.creating && 
-               <Modal title="Add Event" canCancel canConfirm onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}> 
-                    <form>
-                        <div className="form-control">
-                            <label htmlFor="title">Title</label>
-                            <input type="text" id="title" ref={this.titleElRef} />
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="price">Price</label>
-                            <input type="number" id="price" ref={this.priceElRef} />
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="date">Date</label>
-                            <input type="date" id="date" ref={this.dateElRef} />
-                        </div>
-                        <div className="form-control">
-                            <label htmlFor="description">Description</label>
-                            <textarea id="description" rows="4" ref={this.descriptionElRef} />
-                        </div>
-                    </form>
-                </Modal>}
-            <div className="events-control">
-                <p>Share you own Events!</p>
-               <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
-            </div>
-            </React.Fragment>
-        )
+    if (
+    title.trim().length === 0 || 
+    price.trim().length === 0 || 
+    date.trim().length === 0 || 
+    description.trim().length === 0
+    ) {
+        return;
     }
+
+    const event = {title, price, date, description};
+    console.log(event);
+  };
+
+  modalCancelHandler = () => {
+    this.setState({ creating: false });
+  };
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop />}
+
+        {this.state.creating && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <form>
+              <div className="form-control">
+                <label htmlFor="title">Title</label>
+                <input type="text" id="title" ref={this.titleElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="price">Price</label>
+                <input type="number" id="price" ref={this.priceElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="date">Date</label>
+                <input type="date" id="date" ref={this.dateElRef} />
+              </div>
+              <div className="form-control">
+                <label htmlFor="description">Description</label>
+                <textarea
+                  id="description"
+                  rows="4"
+                  ref={this.descriptionElRef}
+                />
+              </div>
+            </form>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share you own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default EventsPage;

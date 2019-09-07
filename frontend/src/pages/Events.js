0213366fd085561cf9fb21fdcc19f365bf1bc530
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-06 23:49:23 -0400

    added an empty array events to state. Added a lifecycle method to call fetchevents so when the page loads it lists out our created events. Created fetch events method to query those events. Finally made a variable eventList to map through the events and created a li with the event title

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import Modal from "../components/Modal/Modal";
 import Backdrop from "../components/Backdrop/Backdrop";
-import AuthContext from '../context/auth-context'
+import AuthContext from "../context/auth-context";
 import "./events.css";
 
 class EventsPage extends Component {
   state = {
-    creating: false
+    creating: false,
+    events: new Array()
   };
 
   static contextType = AuthContext;
@@ -19,7 +20,9 @@ class EventsPage extends Component {
     this.descriptionElRef = React.createRef();
   }
 
-  
+  componentDidMount() {
+    this.fetchEvents();
+  }
 
   startCreateEventHandler = () => {
     this.setState({ creating: true });
@@ -34,21 +37,19 @@ class EventsPage extends Component {
     const description = this.dateElRef.current.value;
 
     if (
-    title.trim().length === 0 || 
-    price <= 0 || 
-    date.trim().length === 0 || 
-    description.trim().length === 0
+      title.trim().length === 0 ||
+      price <= 0 ||
+      date.trim().length === 0 ||
+      description.trim().length === 0
     ) {
-        return;
+      return;
     }
 
-    const event = {title, price, date, description};
+    const event = { title, price, date, description };
     console.log(event);
 
-
-     
-      const requestBody = {
-          query: `
+    const requestBody = {
+      query: `
                   mutation {
                       createEvent(eventInput: {title: "${title}", description: "${description}", price: ${price}, date: "${date}"}) {
                           _id
@@ -63,37 +64,87 @@ class EventsPage extends Component {
                       }
                   }
                    `
-        };
-      
-        const token = this.context.token;
-  
-      fetch("http://localhost:4000/graphql", {
-        method: "POST",
-        body: JSON.stringify(requestBody),
-        headers: {
-          "Content-Type": "application/json",
-          "Authorization": "Bearer " + token
-        }
-      })
-        .then(res => {
-          if (res.status !== 200 && res.status !== 201) {
-            throw new Error("Failed");
-          }
-          return res.json();
-        })
-        .then(resData => {
-         console.log(resData)
-        })
-        .catch(err => {
-          console.log(err);
-        });
     };
-  
+
+    const token = this.context.token;
+
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
   modalCancelHandler = () => {
     this.setState({ creating: false });
   };
+
+  fetchEvents() {
+    const requestBody = {
+      query: `
+                    query {
+                        events {
+                            _id
+                            title
+                            description
+                            date
+                            price
+                            creator {
+                                _id
+                                email
+                            }
+                        }
+                    }
+                     `
+    };
+
+    const token = this.context.token;
+
+    fetch("http://localhost:4000/graphql", {
+      method: "POST",
+      body: JSON.stringify(requestBody),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error("Failed");
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const events = resData.data.events;
+        this.setState({ events: events });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
   render() {
+    const eventList = this.state.events.map(event => {
+      return (
+        <li key={event._id} className="event__list-tipe">
+          {event.title}
+        </li>
+      );
+    });
     return (
       <React.Fragment>
         {this.state.creating && <Backdrop />}
@@ -130,12 +181,17 @@ class EventsPage extends Component {
             </form>
           </Modal>
         )}
-        {this.context.token && <div className="events-control">
-          <p>Share you own Events!</p>
-          <button className="btn" onClick={this.startCreateEventHandler}>
-            Create Event
-          </button>
-        </div>}
+        {this.context.token && (
+          <div className="events-control">
+            <p>Share you own Events!</p>
+            <button className="btn" onClick={this.startCreateEventHandler}>
+              Create Event
+            </button>
+          </div>
+        )}
+        <ul className="events__list">
+        {eventList}
+        </ul>
       </React.Fragment>
     );
   }

7f6444c0d8a19bb7de0bf49beb38d344c134ef58
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-07 21:49:57 -0400

    added ternary operator to switch to View Details or a paragraph tag based on if the userId matches the creatorId

--- frontend/src/components/Events/EventList/EventItem/EventItem.js
+++ frontend/src/components/Events/EventList/EventItem/EventItem.js
@@ -1,18 +1,20 @@
 import React from "react";
 
-import './EventItem.css'
+import "./EventItem.css";
 
 const eventItem = props => (
   <li key={props.eventId} className="events__list-item">
-   <div>
-       <h1>{props.title}</h1>
-       <h2>$19.99</h2>
-   </div>
-   <div>
-       <button>View Details</button>
-       <p>You're the owner of this event</p>
-   </div>
-        
+    <div>
+      <h1>{props.title}</h1>
+      <h2>$19.99</h2>
+    </div>
+    <div>
+      {props.userId === props.creatorId ? (
+        <p>You're the owner of this event</p>
+      ) : (
+        <button className="btn">View Details</button>
+      )}
+    </div>
   </li>
 );
 

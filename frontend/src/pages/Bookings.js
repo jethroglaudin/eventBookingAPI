1bb64d02384806de9ae61e6b0d52b39c52e09035
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-09 22:03:27 -0400

    updated bookings with new state and fetch request

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,6 +1,59 @@
 import React, { Component } from 'react'
 
  class BookingsPage extends Component {
+     state ={
+         isLoading: false,
+         bookings: []
+     }
+     componentDidMount(){
+
+     }
+
+     fetchBookings = () => {
+        const requestBody = {
+            query: `
+                query {
+                  events {
+                    _id
+                    title
+                    description
+                    date
+                    price
+                    creator {
+                      _id
+                      email
+                    }
+                  }
+                }
+              `
+          };
+      
+          fetch('http://localhost:4000/graphql', {
+            method: 'POST',
+            body: JSON.stringify(requestBody),
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          })
+            .then(res => {
+              if (res.status !== 200 && res.status !== 201) {
+                throw new Error('Failed!');
+              }
+              return res.json();
+            })
+            .then(resData => {
+              const events = resData.data.events;
+              if (this.isActive) {
+                this.setState({ events: events, isLoading: false });
+              }
+            })
+            .catch(err => {
+              console.log(err);
+              if (this.isActive) {
+                this.setState({ isLoading: false });
+              }
+            });
+     }
     render() {
         return (
             <div>

dca6f142504417b8feb4381ee26e6a339a11e4c6
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-12 12:51:18 -0400

    added Authorization Bearer token to the http headers

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -1,68 +1,81 @@
-import React, { Component } from 'react'
+  
+import React, { Component } from 'react';
 
- class BookingsPage extends Component {
-     state ={
-         isLoading: false,
-         bookings: []
-     }
-     componentDidMount(){
-      this.fetchBookings();
-     }
+import Spinner from '../components/Spinner/Spinner';
+import AuthContext from '../context/auth-context';
 
-     fetchBookings = () => {
-       this.setState({isLoading: true});
-        const requestBody = {
-            query: `
-                query {
-                  bookings {
-                    _id
-                    createdAt
-                    event {
-                      _id
-                      title
-                      date
-                    }
-                    }
-                  }
-                }
-              `
-          };
-      
-          fetch('http://localhost:4000/graphql', {
-            method: 'POST',
-            body: JSON.stringify(requestBody),
-            headers: {
-              'Content-Type': 'application/json'
+class BookingsPage extends Component {
+  state = {
+    isLoading: false,
+    bookings: []
+  };
+
+  static contextType = AuthContext;
+
+  componentDidMount() {
+    this.fetchBookings();
+  }
+
+  fetchBookings = () => {
+    this.setState({ isLoading: true });
+    const requestBody = {
+      query: `
+          query {
+            bookings {
+              _id
+             createdAt
+             event {
+               _id
+               title
+               date
+             }
             }
-          })
-            .then(res => {
-              if (res.status !== 200 && res.status !== 201) {
-                throw new Error('Failed!');
-              }
-              return res.json();
-            })
-            .then(resData => {
-              const bookings = resData.data.bookings;
-              if (this.isActive) {
-                this.setState({ bookings: bookings, isLoading: false });
-              }
-            })
-            .catch(err => {
-              console.log(err);
-              if (this.isActive) {
-                this.setState({ isLoading: false });
-              }
-            });
-     }
-    render() {
-        return (
-            <div>
-               <ul>
-                 {this.state.bookings.map(booking => <li>{booking.event.title}  -  {new Date(booking.createdAt).toLocaleDateString()}</li>)}
-               </ul>
-            </div>
-        )
-    }
+          }
+        `
+    };
+
+    fetch('http://localhost:4000/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + this.context.token
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        const bookings = resData.data.bookings;
+        this.setState({ bookings: bookings, isLoading: false });
+      })
+      .catch(err => {
+        console.log(err);
+        this.setState({ isLoading: false });
+      });
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        {this.state.isLoading ? (
+          <Spinner />
+        ) : (
+          <ul>
+            {this.state.bookings.map(booking => (
+              <li key={booking._id}>
+                {booking.event.title} -{' '}
+                {new Date(booking.createdAt).toLocaleDateString()}
+              </li>
+            ))}
+          </ul>
+        )}
+      </React.Fragment>
+    );
+  }
 }
 
 export default BookingsPage;

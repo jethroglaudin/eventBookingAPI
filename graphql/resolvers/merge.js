edc36b3684b92c9e605b5ca6c3ba90952dd2f68e
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-12 12:45:12 -0400

    updated merge resolvers

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -3,64 +3,61 @@ const User = require('../../models/user');
 const { dateToString } = require('../../helpers/date');
 
 const events = async eventIds => {
-    // look for through events where ID is in a list of ids.
-    try {
-      const events = await Event.find({ _id: { $in: eventIds } });
-      return events.map(event => {
-        return transformEvent(event);
-      });
-    } catch (err) {
-      throw err;
-    }
-  };
-  
-  const singleEvent = async eventId => {
-    try {
-      const event = await Event.findById(eventId);
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
       return transformEvent(event);
-    } catch (err) {
-      throw err;
-    }
-  };
+    });
+  } catch (err) {
+    throw err;
+  }
+};
 
-  
-  
-  const user = async userId => {
-    try {
-      const user = await User.findById(userId);
-      return {
-        ...user._doc,
-        _id: user.id,
-        createdEvents: events.bind(this, user.createdEvents)
-      };
-    } catch (err) {
-      throw err;
-    }
-  };
+const singleEvent = async eventId => {
+  try {
+    const event = await Event.findById(eventId);
+    return transformEvent(event);
+  } catch (err) {
+    throw err;
+  }
+};
 
-  const transformEvent = event => {
+const user = async userId => {
+  try {
+    const user = await User.findById(userId);
     return {
-      ...event._doc,
-      _id: event.id,
-      date: dateToString(event._doc.date),
-      creator: user.bind(this, event.creator)
+      ...user._doc,
+      _id: user.id,
+      createdEvents: events.bind(this, user._doc.createdEvents)
     };
-  };
+  } catch (err) {
+    throw err;
+  }
+};
 
-  const transformBooking = booking => {
-    return {
-      ...booking._doc,
-      _id: booking.id,
-      user: user.bind(this, booking.user),
-      event: singleEvent.bind(this, booking._doc.event),
-      createdAt: dateToString(booking.createdAt),
-      updatedAt: dateToString(booking.updatedAt)
-    };
+const transformEvent = event => {
+  return {
+    ...event._doc,
+    _id: event.id,
+    date: dateToString(event._doc.date),
+    creator: user.bind(this, event.creator)
   };
-  
-  exports.transformEvent = transformEvent;
-  exports.transformBooking = transformBooking;
+};
 
-//   exports.user = user;
-//    exports.events = events;
-//   exports.singleEvent = singleEvent;
+const transformBooking = booking => {
+  return {
+    ...booking._doc,
+    _id: booking.id,
+    user: user.bind(this, booking._doc.user),
+    event: singleEvent.bind(this, booking._doc.event),
+    createdAt: dateToString(booking._doc.createdAt),
+    updatedAt: dateToString(booking._doc.updatedAt)
+  };
+};
+
+exports.transformEvent = transformEvent;
+exports.transformBooking = transformBooking;
+
+// exports.user = user;
+// exports.events = events;
+// exports.singleEvent = singleEvent;

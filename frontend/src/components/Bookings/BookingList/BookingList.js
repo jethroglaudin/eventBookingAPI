875cacc3e95b1ffc7d609af40e3e5ae5465ae095
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-13 20:21:42 -0400

    updated bookinglist. added key which is the booking id

--- frontend/src/components/Bookings/BookingList/BookingList.js
+++ frontend/src/components/Bookings/BookingList/BookingList.js
@@ -1,19 +1,24 @@
-import React from "react";
+  
+import React from 'react';
+
+import './BookingList.css';
 
 const bookingList = props => (
-  <ul>
+  <ul className="bookings__list">
     {props.bookings.map(booking => {
       return (
-        <li className="bookings_item">
-          <div className="bookings_item-data">
-            {booking.event.title} -{" "}
+        <li key={booking._id} className="bookings__item">
+          <div className="bookings__item-data">
+            {booking.event.title} -{' '}
             {new Date(booking.createdAt).toLocaleDateString()}
           </div>
           <div className="bookings__item-actions">
-              <button>Cancel</button>
+              <button className="btn">Cancel</button>
           </div>
         </li>
       );
     })}
   </ul>
 );
+
+export default bookingList;

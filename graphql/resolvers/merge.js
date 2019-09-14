0b6c82cd76cf117b0008db63d76f01dbc01342aa
Jethro Glaudin <jethro_glaudin@my.uri.edu>
2019-09-13 22:33:59 -0400

    updated merge.js

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -9,12 +9,16 @@ const eventLoader = new DataLoader((eventIds) => {
 
 });
 
+const userLoader = new DataLoader((userIds => {
+  return User.find({_id: {$in: userIds}});
+}))
+
 const events = async eventIds => {
   try {
     const events = await Event.find({ _id: { $in: eventIds } });
     return events.map(event => {
       return transformEvent(event);
-    });
+    }); 
   } catch (err) {
     throw err;
   }
@@ -22,7 +26,7 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
   try {
-    const event = await eventLoader.load(eventId);
+    const event = await eventLoader.load(eventId.toString());
     return event;
   } catch (err) {
     throw err;
@@ -31,7 +35,7 @@ const singleEvent = async eventId => {
 
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc,
       _id: user.id,
